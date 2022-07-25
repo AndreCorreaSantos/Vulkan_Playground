63814d93ec46d21b63f847f52bfbc678676277b9
AndreCorreaSantos <acorreasantos2002@gmail.com>
2022-07-25 15:03:17 -0300

    funciona

--- lve_model.hpp
+++ lve_model.hpp
@@ -14,14 +14,19 @@ namespace lve {
 class LveModel {
  public:
   struct Vertex {
-    glm::vec3 position;
-    glm::vec3 color;
+    glm::vec3 position{};
+    glm::vec3 color{};
 
     static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
     static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
   };
 
-  LveModel(LveDevice &device, const std::vector<Vertex> &vertices);
+  struct Builder {
+    std::vector<Vertex> vertices{};
+    std::vector<uint32_t> indices{};
+  };
+
+  LveModel(LveDevice &device, const LveModel::Builder &builder);
   ~LveModel();
 
   LveModel(const LveModel &) = delete;
@@ -32,10 +37,17 @@ class LveModel {
 
  private:
   void createVertexBuffers(const std::vector<Vertex> &vertices);
+  void createIndexBuffers(const std::vector<uint32_t> &indices);
 
   LveDevice &lveDevice;
+
   VkBuffer vertexBuffer;
   VkDeviceMemory vertexBufferMemory;
   uint32_t vertexCount;
+
+  bool hasIndexBuffer = false;
+  VkBuffer indexBuffer;
+  VkDeviceMemory indexBufferMemory;
+  uint32_t indexCount;
 };
 }  // namespace lve
